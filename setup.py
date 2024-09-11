7e26af1093134d12cb8f4107009d3510ed5e5560
jgravelle <j@gravelle.us>
2024-09-11 14:12:56 -0500

    refactor: Increment version number to 0.2.4 in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pocketgroq",
-    version="0.2.4",  # Increment the version number
+    version="0.2.5",  # Increment the version number
     author="PocketGroq Team",
     author_email="pocketgroq@example.com",
     description="A library for easy integration with Groq API, including image handling",
